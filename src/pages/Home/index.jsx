bae76cda2d30ad6cf88e3187291e4a54e806c276
Hallan.Neves <hallanneves01@gmail.com>
2023-03-31 15:46:50 -0300

    Primeiro commit projeto lista de presença

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+export default function Home() {
+    return (
+        <div className='container'>
+            <h1>Lista de presença</h1>
+            <input type="text" placeholder="Digite o nome..." />
+            <button type="button">Adicionar</button>
+        </div>
+    );
+}
