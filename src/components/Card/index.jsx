2d41adb63186be1e2073693069239b4e024b830f
hallanneves <hallanneves01@gmail.com>
2023-03-31 21:23:36 -0300

    Estruturando o projeto

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+
+
+export default function Card({name, time}) {
+    return (
+        <div className="card">
+            <strong>{name}</strong>
+            <small>{time}</small>
+        </div>
+    );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,24 @@
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
+import Card from "../../components/Card";
+
 export default function Home() {
+
+    const [studentName, setStudentName] = useState()
+
     return (
-        <div className='container'>
-            <h1>Lista de presença</h1>
-            <input type="text" placeholder="Digite o nome..." />
+        <div className="container">
+            <h1>Name: {studentName}</h1>
+            <input
+                type="text"
+                placeholder="Digite o nome..."
+                onChange={(e) => setStudentName(e.target.value)}
+            />
             <button type="button">Adicionar</button>
+
+            <Card name="João" time="15:35:20" />
+            <Card name="Hállan" time="16:35:20" />
+            <Card name="Mara" time="18:35:20" />
         </div>
     );
 }
